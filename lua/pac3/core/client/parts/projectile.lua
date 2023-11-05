5cdf272d8ea22197d6bdc79c1c7ea39308f712f0
pingu7867 <cedric4321@hotmail.com>
2023-11-05 15:46:20 +0800

    extend projectile part\nAdd surface properties\nAbility to change invisible physics model\nSome experimental properties\nand more

--- lua/pac3/core/client/parts/projectile.lua
+++ lua/pac3/core/client/parts/projectile.lua
@@ -1,24 +1,58 @@
+local physprop_enums = {}
+local physprop_indices = {}
+for i=0,200,1 do
+	local name = util.GetSurfacePropName(i)
+	if name ~= "" then
+		physprop_enums[name] = name
+		physprop_indices[name] = i
+	end
+end
+
 language.Add("pac_projectile", "Projectile")
+
+
 
 local BUILDER, PART = pac.PartTemplate("base_movable")
 
 PART.ClassName = "projectile"
-PART.Group = 'advanced'
-PART.Icon = 'icon16/bomb.png'
+PART.Group = "advanced"
+PART.Icon = "icon16/bomb.png"
 
 BUILDER:StartStorableVars()
+	BUILDER:SetPropertyGroup("Firing")
 	BUILDER:GetSet("Speed", 1)
 	BUILDER:GetSet("AddOwnerSpeed", false)
+		BUILDER:GetSet("Spread", 0)
+		BUILDER:GetSet("NumberProjectiles", 1)
+		BUILDER:GetSet("Delay", 0)
+		BUILDER:GetSet("Maximum", 0)
+		BUILDER:GetSet("RandomAngleVelocity", Vector(0,0,0))
+		BUILDER:GetSet("LocalAngleVelocity", Vector(0,0,0))
+	BUILDER:SetPropertyGroup("Physics")
+		BUILDER:GetSet("Mass", 100)
+		BUILDER:GetSet("SurfaceProperties", "default", {enums = physprop_enums})
+		BUILDER:GetSet("RescalePhysMesh", false, {description = "experimental! tries to scale the collide mesh by the radius! Stay within  small numbers! 1 radius should be associated with a full-size model"})
+		BUILDER:GetSet("OverridePhysMesh", false, {description = "experimental! tries to redefine the projectile's model to change the physics mesh"})
+		BUILDER:GetSet("FallbackSurfpropModel", "models/props_junk/PopCan01a.mdl")
 	BUILDER:GetSet("Damping", 0)
 	BUILDER:GetSet("Gravity", true)
 	BUILDER:GetSet("Collisions", true)
 	BUILDER:GetSet("Sphere", false)
-	BUILDER:GetSet("Radius", 1)
-	BUILDER:GetSet("DamageRadius", 50)
-	BUILDER:GetSet("LifeTime", 5)
+		BUILDER:GetSet("Radius", 1, {editor_panel = "projectile_radii"})
+		BUILDER:GetSet("Bounce", 0)
+		BUILDER:GetSet("Sticky", false)
+		BUILDER:GetSet("CollideWithOwner", false)
+		BUILDER:GetSet("CollideWithSelf", false)
+	BUILDER:SetPropertyGroup("Appearance")
+		BUILDER:GetSetPart("OutfitPart")
+		BUILDER:GetSet("RemoveOnHide", false)
 	BUILDER:GetSet("AimDir", false)
-	BUILDER:GetSet("Sticky", false)
-	BUILDER:GetSet("Bounce", 0)
+		BUILDER:GetSet("DrawShadow", true)
+	BUILDER:SetPropertyGroup("ActiveBehavior")
+		BUILDER:GetSet("Physical", false)
+		BUILDER:GetSet("DamageRadius", 50, {editor_panel = "projectile_radii"})
+		BUILDER:GetSet("LifeTime", 5)
+		BUILDER:GetSet("RemoveOnCollide", false)
 	BUILDER:GetSet("BulletImpact", false)
 	BUILDER:GetSet("Damage", 0)
 	BUILDER:GetSet("DamageType", "generic", {enums = {
@@ -67,23 +101,15 @@ BUILDER:StartStorableVars()
 			armor = -1,
 		}
 	})
-	BUILDER:GetSet("Spread", 0)
-	BUILDER:GetSet("Delay", 0)
-	BUILDER:GetSet("Maximum", 0)
-	BUILDER:GetSet("Mass", 100)
-	BUILDER:GetSet("Attract", 0)
-	BUILDER:GetSet("AttractMode", "projectile_nearest", {enums = {
+		BUILDER:GetSet("Attract", 0, {editor_friendly = "attract force"})
+		BUILDER:GetSet("AttractMode", "closest_to_projectile", {enums = {
 		hitpos = "hitpos",
 		hitpos_radius = "hitpos_radius",
 		closest_to_projectile = "closest_to_projectile",
 		closest_to_hitpos = "closest_to_hitpos",
 	}})
 	BUILDER:GetSet("AttractRadius", 200)
-	BUILDER:GetSetPart("OutfitPart")
-	BUILDER:GetSet("Physical", false)
-	BUILDER:GetSet("CollideWithOwner", false)
-	BUILDER:GetSet("CollideWithSelf", false)
-	BUILDER:GetSet("RemoveOnCollide", false)
+
 BUILDER:EndStorableVars()
 
 PART.Translucent = false
@@ -103,11 +129,20 @@ function PART:OnShow(from_rendering)
 				part:Draw("opaque")
 			end
 		end
-		self:Shoot(self:GetDrawPosition())
+		if self.NumberProjectiles <= 0 then self.NumberProjectiles = 0 end
+		if self.NumberProjectiles <= 50 then
+			local pos,ang = self:GetDrawPosition()
+			self:Shoot(pos,ang,self.NumberProjectiles)
+		else chat.AddText(Color(255,0,0),"[PAC3] Trying to spawn too many projectiles! The limit is " .. 50) end
 	end
 end
 
-function PART:AttachToEntity(ent)
+function PART:GetSurfacePropsTable() --to view info over in the properties
+	return util.GetSurfaceData(physprop_indices[self.SurfaceProperties])
+end
+
+local_projectiles = {}
+function PART:AttachToEntity(ent, physical)
 	if not self.OutfitPart:IsValid() then return false end
 
 	ent.pac_draw_distance = 0
@@ -140,28 +175,120 @@ function PART:AttachToEntity(ent)
 	end
 
 	ent.pac_projectile_part = group
-	ent.pac_projectile = self
+	ent.pac_projectile = self --that's just the launcher though
+	if not physical then local_projectiles[group] = ent end
 
 	return true
 end
 
 local enable = CreateClientConVar("pac_sv_projectiles", 0, true)
 
-function PART:Shoot(pos, ang)
+local damage_ids = {
+	generic = 0, --generic damage
+	crush = 1, --caused by physics interaction
+	bullet = 2, --bullet damage
+	slash = 3, --sharp objects, such as manhacks or other npcs attacks
+	burn = 4, --damage from fire
+	vehicle = 5, --hit by a vehicle
+	fall = 6, --fall damage
+	blast = 7, --explosion damage
+	club = 8, --crowbar damage
+	shock = 9, --electrical damage, shows smoke at the damage position
+	sonic = 10, --sonic damage,used by the gargantua and houndeye npcs
+	energybeam = 11, --laser
+	nevergib = 12, --don't create gibs
+	alwaysgib = 13, --always create gibs
+	drown = 14, --drown damage
+	paralyze = 15, --same as dmg_poison
+	nervegas = 16, --neurotoxin damage
+	poison = 17, --poison damage
+	acid = 18, --
+	airboat = 19, --airboat gun damage
+	blast_surface = 20, --this won't hurt the player underwater
+	buckshot = 21, --the pellets fired from a shotgun
+	direct = 22, --
+	dissolve = 23, --forces the entity to dissolve on death
+	drownrecover = 24, --damage applied to the player to restore health after drowning
+	physgun = 25, --damage done by the gravity gun
+	plasma = 26, --
+	prevent_physics_force = 27, --
+	radiation = 28, --radiation
+	removenoragdoll = 29, --don't create a ragdoll on death
+	slowburn = 30, --
+
+	explosion = 31, -- ent:Ignite(5)
+	fire = 32, -- ent:Ignite(5)
+
+	-- env_entity_dissolver
+	dissolve_energy = 33,
+	dissolve_heavy_electrical = 34,
+	dissolve_light_electrical = 35,
+	dissolve_core_effect = 36,
+
+	heal = 37,
+	armor = 38,
+}
+local attract_ids = {
+	hitpos = 0,
+	hitpos_radius = 1,
+	closest_to_projectile = 2,
+	closest_to_hitpos = 3,
+}
+function PART:Shoot(pos, ang, multi_projectile_count)
 	local physics = self.Physical
+	local multi_projectile_count = multi_projectile_count or 1
 
 	if physics then
 		if pac.LocalPlayer ~= self:GetPlayerOwner() then return end
 
 		local tbl = {}
-		for key in pairs(self:GetStorableVars()) do
-			tbl[key] = self[key]
-		end
 
-		net.Start("pac_projectile")
+		net.Start("pac_projectile",true)
+			net.WriteUInt(multi_projectile_count,7)
 			net.WriteVector(pos)
 			net.WriteAngle(ang)
-			net.WriteTable(tbl)
+
+			--bools
+			net.WriteBool(self.Sphere)
+			net.WriteBool(self.RemoveOnCollide)
+			net.WriteBool(self.CollideWithOwner)
+			net.WriteBool(self.RemoveOnHide)
+			net.WriteBool(self.OverridePhysMesh)
+			net.WriteBool(self.Gravity)
+			net.WriteBool(self.AddOwnerSpeed)
+			net.WriteBool(self.Collisions)
+			net.WriteBool(self.CollideWithSelf)
+			net.WriteBool(self.AimDir)
+			net.WriteBool(self.DrawShadow)
+			net.WriteBool(self.Sticky)
+			net.WriteBool(self.BulletImpact)
+
+			--vectors
+			net.WriteVector(self.RandomAngleVelocity)
+			net.WriteVector(self.LocalAngleVelocity)
+
+			--strings
+			net.WriteString(self.OverridePhysMesh and string.sub(string.gsub(self.FallbackSurfpropModel, "^models/", ""),1,150) or "") --custom model is an unavoidable string
+			net.WriteString(string.sub(self.UniqueID,1,12)) --long string but we can probably truncate it
+			net.WriteUInt(physprop_indices[self.SurfaceProperties] or 0,10)
+			net.WriteUInt(damage_ids[self.DamageType] or 0,7)
+			net.WriteUInt(attract_ids[self.AttractMode] or 2,3)
+
+			--numbers
+			net.WriteUInt(self.Radius,8)
+			net.WriteUInt(self.DamageRadius,10)
+			net.WriteUInt(self.Damage,24)
+			net.WriteUInt(1000*self.Speed,16)
+			net.WriteUInt(self.Maximum,7)
+			net.WriteUInt(100*self.LifeTime,14) --might need decimals
+			net.WriteUInt(100*self.Delay,9) --might need decimals
+			net.WriteUInt(self.Mass,18)
+			net.WriteInt(100*self.Spread,10)
+			net.WriteInt(100*self.Damping,20) --might need decimals
+			net.WriteInt(self.Attract,14)
+			net.WriteUInt(self.AttractRadius,10)
+			net.WriteInt(100*self.Bounce,8) --might need decimals
+
 		net.SendToServer()
 	else
 		self.projectiles = self.projectiles or {}
@@ -190,7 +317,7 @@ function PART:Shoot(pos, ang)
 
 			if not self:IsValid() then return end
 
-			local ent = pac.CreateEntity("models/props_junk/popcan01a.mdl")
+			local ent = pac.CreateEntity(self.FallbackSurfpropModel)
 			if not ent:IsValid() then return end
 
 			local idx = table.insert(self.projectiles, ent)
@@ -232,9 +359,9 @@ function PART:Shoot(pos, ang)
 			ent:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
 
 			if self.Sphere then
-				ent:PhysicsInitSphere(math.Clamp(self.Radius, 1, 30))
+				ent:PhysicsInitSphere(math.Clamp(self.Radius, 1, 500), self.SurfaceProperties)
 			else
-				ent:PhysicsInitBox(Vector(1,1,1) * - math.Clamp(self.Radius, 1, 30), Vector(1,1,1) * math.Clamp(self.Radius, 1, 30))
+				ent:PhysicsInitBox(Vector(1,1,1) * - math.Clamp(self.Radius, 1, 500), Vector(1,1,1) * math.Clamp(self.Radius, 1, 500), self.SurfaceProperties)
 			end
 
 			ent.RenderOverride = function()
@@ -268,7 +395,8 @@ function PART:Shoot(pos, ang)
 
 			ent:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
 
-			if self:AttachToEntity(ent) then
+			if self:AttachToEntity(ent, false) then
+
 				timer.Simple(math.Clamp(self.LifeTime, 0, 10), function()
 					if ent:IsValid() then
 						if ent.pac_projectile_part and ent.pac_projectile_part:IsValid() then
@@ -280,33 +408,97 @@ function PART:Shoot(pos, ang)
 						end)
 					end
 				end)
+
 			end
+
+
 		end
 
 		if self.Delay == 0 then
+			for i = multi_projectile_count,1,-1 do
 			spawn()
+			end
 		else
 			timer.Simple(self.Delay, spawn)
 		end
 	end
 end
 
-function PART:OnRemove()
-	if not self.Physical and self.projectiles then
-		for key, ent in pairs(self.projectiles) do
-			SafeRemoveEntity(ent)
-		end
+function PART:SetRadius(val)
+	self.Radius = val
+	local sv_dist = GetConVar("pac_sv_projectile_max_phys_radius"):GetInt()
+	if self.Radius > sv_dist then
+		self:SetInfo("Your radius is beyond the server's maximum permitted! Server max is " .. sv_dist)
+	else
+		self:SetInfo(nil)
+	end
+end
 
-		self.projectiles = {}
+function PART:SetDamageRadius(val)
+	self.DamageRadius = val
+	local sv_dist = GetConVar("pac_sv_projectile_max_damage_radius"):GetInt()
+	if self.DamageRadius > sv_dist then
+		self:SetInfo("Your damage radius is beyond the server's maximum permitted! Server max is " .. sv_dist)
+	else
+		self:SetInfo(nil)
 	end
 end
---[[
-function PART:OnHide()
-	if self.RemoveOnHide then
-		self:OnRemove()
+
+function PART:SetAttractRadius(val)
+	self.AttractRadius = val
+	local sv_dist = GetConVar("pac_sv_projectile_max_attract_radius"):GetInt()
+	if self.AttractRadius > sv_dist then
+		self:SetInfo("Your attract radius is beyond the server's maximum permitted! Server max is " .. sv_dist)
+	else
+		self:SetInfo(nil)
 	end
 end
-]]
+
+function PART:SetSpeed(val)
+	self.Speed = val
+	local sv_max = GetConVar("pac_sv_projectile_max_speed"):GetInt()
+	if self.Speed > sv_max then
+		self:SetInfo("Your speed is beyond the server's maximum permitted! Server max is " .. sv_max)
+	else
+		self:SetInfo(nil)
+	end
+end
+
+function PART:SetMass(val)
+	self.Mass = val
+	local sv_max = GetConVar("pac_sv_projectile_max_mass"):GetInt()
+	if self.Mass > sv_max then
+		self:SetInfo("Your mass is beyond the server's maximum permitted! Server max is " .. sv_max)
+	else
+		self:SetInfo(nil)
+	end
+end
+
+function PART:SetDamage(val)
+	self.Damage = val
+	local sv_max = GetConVar("pac_sv_damage_zone_max_damage"):GetInt()
+	if self.Damage > sv_max then
+		self:SetInfo("Your damage is beyond the server's maximum permitted! Server max is " .. sv_max)
+	else
+		self:SetInfo(nil)
+	end
+end
+
+pac.AddHook("Think", "pac_cleanup_CS_projectiles", function()
+	for rootpart,ent in pairs(local_projectiles) do
+		if ent.pac_projectile_part == rootpart then
+			local tbl = ent.pac_projectile_part:GetChildren()
+			local partchild = tbl[next(tbl)] --ent.pac_projectile_part is the root group, but outfit part is the first child
+
+			if IsValid(partchild) then
+				if partchild:IsHidden() then
+					SafeRemoveEntity(ent)
+				end
+			end
+		end
+	end
+end)
+
 do -- physical
 	local Entity = Entity
 	local projectiles = {}
@@ -320,9 +512,9 @@ do -- physical
 			local ent = Entity(data.ent_id)
 
 			if ent:IsValid() and ent:GetClass() == "pac_projectile" then
-				local part = pac.GetPartFromUniqueID(pac.Hash(data.ply), data.partuid)
+				local part = pac.FindPartByPartialUniqueID(pac.Hash(data.ply), data.partuid)
 				if part:IsValid() and part:GetPlayerOwner() == data.ply then
-					part:AttachToEntity(ent)
+					part:AttachToEntity(ent, true)
 				end
 				projectiles[key] = nil
 			end
@@ -334,9 +526,31 @@ do -- physical
 		local ply = net.ReadEntity()
 		local ent_id = net.ReadInt(16)
 		local partuid = net.ReadString()
+		local surfprop = net.ReadString()
 
 		if ply:IsValid() then
 			table.insert(projectiles, {ply = ply, ent_id = ent_id, partuid = partuid})
+		end
+
+		local ent = Entity(ent_id)
+
+		ent.Think = function()
+			if ent.pac_projectile_part then
+				local tbl = ent.pac_projectile_part:GetChildren()
+				local partchild = tbl[next(tbl)] --ent.pac_projectile_part is the root group, but outfit part is the first child
+				if IsValid(partchild) then
+					if partchild:IsHidden() then
+						if ent.pac_projectile.RemoveOnHide and not ent.markedforremove then
+							ent.markedforremove = true
+							net.Start("pac_projectile_remove")
+							net.WriteInt(ent_id, 16)
+							net.SendToServer()
+
+						end
+					end
+				end
+
+			end
 		end
 	end)
 end
